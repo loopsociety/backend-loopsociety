25e971130177b8c05450cb0f8bb6a6d48939eadc
sdilone <dilonealcantara@gmail.com>
2025-07-23 22:04:03 -0400

    refactor: improve session management in get_session function

--- app/db/database.py
+++ app/db/database.py
@@ -1,10 +1,12 @@
 from sqlmodel import Session, create_engine
 from app.core.config import DATABASE_URL
 
-
 engine = create_engine(DATABASE_URL, echo=True)
 
 
 def get_session():
-    with Session(engine) as session:
+    session = Session(engine)
+    try:
         yield session
+    finally:
+        session.close()
